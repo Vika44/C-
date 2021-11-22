440b465ca86873639458a5ef77e871abb80243f6
Viktoria <vikulik.gh@gmail.com>
2021-11-23 01:03:32 +0400

    Variable and Constant

---
+++ CSharp_lessons/CSharp_basic/Variable_and_constant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variable_and_constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ann"; // define a variable and initialize it
+            Console.WriteLine(name);
+            name = "Lin"; // change the value of the variable
+            Console.WriteLine(name);
+
+
+            const int age = 20; // define a constant
+            // age = 21; // ! Error - the value of a constant cannot be changed
+
+        }
+    }
+}
