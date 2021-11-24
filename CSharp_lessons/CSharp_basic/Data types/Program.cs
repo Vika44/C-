bac8413e22a779bad7813a886cd01bf4bb9fa746
Viktoria <vikulik.gh@gmail.com>
2021-11-25 00:10:42 +0400

    Data types

---
+++ CSharp_lessons/CSharp_basic/Data types/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Anna";
+            int age = 25;
+            bool work = false;
+            double weight = 50.20;
+            Console.WriteLine($" name:{name} \n age:{age} \n work:{work} \n weight:{weight} ");
+
+            // byte-stores an integer from 0 to 255 and takes 1 byte
+
+            // object- can store a value of any data type
+            object a = "Ann";
+            object b = 20;
+
+            // char stores a single character
+            char table = 'u';
+
+            // Implicit typing
+            var system = "windows 11";
+            var ghy = 20;
+
+
+
+
+
+
+
+
+        }
+    }
+}
